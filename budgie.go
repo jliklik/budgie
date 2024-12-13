31c00a40947e5dc882e1a6868dbd76dcfbe25310
Jian Lik Ng <jlik.lik@gmail.com>
2024-12-12 22:18:45 -0800

    Successful connect to mongodb server

--- budgie.go
+++ budgie.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"context"
+	// "encoding/json"
+	// "log"
+	// "os"
+
+	// "github.com/joho/godotenv"
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-  fmt.Println("Hello, world")
+	fmt.Println("Hello, world")
+	uri := "mongodb://127.0.0.1:27017"
+
+	// context.TODO() creates an empty context
+	// options.Client().ApplyURI() is part of mongo-driver/mongo/options package 
+	_, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to mongodb server")
 }
